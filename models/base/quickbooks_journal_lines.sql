bc236599d4bd751c80718bd3bfa45b6d3f8b5a2d
eogilvy12 <erin.ogilvy@gmail.com>
2017-02-16 14:51:12 -0500

    adding casting for account_id

--- models/base/quickbooks_journal_lines.sql
+++ models/base/quickbooks_journal_lines.sql
@@ -2,9 +2,9 @@ select
   --this id is only unique within a given entry_id; may also want to create a globally unique id for this table.
   id::int,
   {{ var('source_key_id_field') }} as entry_id,
-  amount,
+  coalesce(amount::numeric(38,6), 0::numeric(38,6)) as amount,
   description,
-  journalentrylinedetail__accountref__value::integer as account_id,
+  nullif(journalentrylinedetail__accountref__value, '')::integer as account_id,
   nullif(journalentrylinedetail__classref__value::varchar, '')::bigint as class_id,
   journalentrylinedetail__postingtype as posting_type
 from
