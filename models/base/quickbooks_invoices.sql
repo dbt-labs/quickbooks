7f60e5be722018d55bc6213ec6785bd3c90e4268
Tristan Handy <jthandy.gmail.com>
2017-07-01 22:55:40 -0400

    added two invoice fields

--- models/base/quickbooks_invoices.sql
+++ models/base/quickbooks_invoices.sql
@@ -1,18 +1,15 @@
 select
-  id::int,
-  totalamt as total_amt,
-  txndate::date as txn_date,
-  duedate::date as due_date,
-  balance,
-  --einvoicestatus as e_status,
-  --ecloudstatustimestamp::datetime as e_status_timestamp,
-  --deliveryinfo__deliverytype as delivery_type,
-  --deliveryinfo__deliverytime::datetime as delivery_time,
-  -- shipdate?
-  emailstatus as email_status,
-  docnumber as doc_number,
-  customerref__value::int as customer_id,
-  metadata__createtime as created_at,
-  metadata__lastupdatedtime as updated_at
+    id::int,
+    totalamt as total_amt,
+    txndate::date as txn_date,
+    duedate::date as due_date,
+    balance,
+    deliveryinfo__deliverytype as delivery_type,
+    deliveryinfo__deliverytime::datetime as delivery_time,
+    emailstatus as email_status,
+    docnumber as doc_number,
+    customerref__value::int as customer_id,
+    metadata__createtime as created_at,
+    metadata__lastupdatedtime as updated_at
 from
-  {{ var('base.invoices') }}
+    {{ var('base.invoices') }}
