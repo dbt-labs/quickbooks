9a0edf66cb756636ccc4a070ec1cb0da2792e7a6
Tristan Handy <jthandy@gmail.com>
2019-04-24 21:09:30 -0400

    implement utils and clean up project file

--- models/transform/quickbooks_days.sql
+++ models/transform/quickbooks_days.sql
@@ -1,2 +1,5 @@
-select (min(txn_date) over () + row_number() over ())::date as date_day
-from {{ref('quickbooks_general_ledger')}}
+{{ dbt_utils.date_spine(
+    datepart="minute",
+    start_date="(select min(txn_date) from " ~ ref('quickbooks_general_ledger') ~ ")",
+    end_date="dateadd(week, 1, current_date)"
+    )}}
